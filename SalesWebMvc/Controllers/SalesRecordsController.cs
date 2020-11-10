6cfa7b4efa90a4dc247f4ba8de4a959d0bb00763
Allan Souza Silva <allan-souza16@hotmail.com>
2020-11-09 21:14:40 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //METODO DE BUSCA SIMPLES
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        //Metodo de busca agrupada
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
